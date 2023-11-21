35e9184cc8cfa956a3750d2683938cf03d405f1d
Custom Software <kcluff50@gmail.com>
2023-11-21 07:13:10 -0700

    Initial Commit

---
+++ DemoMovies/DemoMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMoviesApp.swift
+//  DemoMovies
+//
+//  Created by Kenneth Cluff on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
